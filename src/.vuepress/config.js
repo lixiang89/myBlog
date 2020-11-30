366f9aaaab7a998ce4fae32447547839815f5e86
yishuihe <739708098@qq.com>
2020-11-30 22:46:58 +0800

    修改一些配置

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     base: '/myBlog/',
     title: '伊水河的博客',
     dest: './docs',
-    description: '随便记录点什么',
+    description: 'SO FAR SO GOOD',
     locales: {
         '/':{
             lang:'zh-CN'
@@ -16,7 +16,8 @@ module.exports = {
                 name:'viewport',
                 content:'width=device-width,initial-scale=1,user-scalable=no'
             }
-        ]
+        ],
+        ['link', { rel: 'icon', href: '/logo.png' }]
     ],
     themeConfig: {
         // sidebar: 'auto',
@@ -25,24 +26,46 @@ module.exports = {
         subSidebar: 'auto',
         authorAvatar: '/b.jpg',
         nav: [
-            { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
+            { 
+                text: '切换', 
+                icon: 'reco-blog',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { text: '码云', link: 'https://yishuihe.gitee.io/myblog', target:'_self', icon: 'reco-mayun' },
+                    { text: 'github', link: 'https://lixiang89.github.io/myBlog/', target:'_self', icon: 'reco-github' },
+                ] 
+            },
             { text: '前端', link: '/categories/前端/', icon: 'reco-api' },
+            { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
+            { text: 'TODO', link: '/todo/', icon: 'reco-suggestion' },
+            { 
+                text: '关于', 
+                icon: 'reco-account',
+                items: [
+                    { text: '掘金', link: 'https://juejin.cn/user/571401777456621', icon: 'reco-juejin' },
+                    { text: 'github', link: 'https://github.com/lixiang89', icon: 'reco-github' },
+                ] 
+            },
         ],
         author: '伊水河',
-        blogConfig: {
+        // blogConfig: {
             // category: {
             //   location: 2,     // 在导航栏菜单中所占的位置，默认2
             // //   text: 'Category' // 默认文案 “分类”
             // },
-            tag: {
-              location: 3,     // 在导航栏菜单中所占的位置，默认3
+            // tag: {
+            //   location: 3,     // 在导航栏菜单中所占的位置，默认3
             //   text: 'Tag'      // 默认文案 “标签”
-            }
-        }
+            // }
+        // },
     },
     plugins: [
-        ['vuepress-plugin-code-copy',{
-            successText: '复制成功！'
+        ["vuepress-plugin-nuggets-style-copy", {
+            copyText: "代码复制",
+            tip: {
+                content: "复制成功!"
+            }
         }]
+          
     ]
 }
---
+++ src/guide/learnGit.md
@@ -0,0 +1,27 @@
+---
+title: git入门学习
+date: 2020-11-30
+categories:
+ - 前端
+tags:
+ - git
+publish: false
+---
+
+虽然git也用了好几年，但一直使用的都是各种图形化界面工具（TortoiseGit小乌龟、Git Extensions、vscode等），除了pull、push之外的命令基本不会敲。遂重学了git基本命令并记录。
+
+## 基础概念
+## git init
+## git commit
+## git reset
+## git revert
+## git branch
+## git checkout
+## git switch
+## git rebase
+## git merge
+## git fetch
+## git pull
+## git push
+## git stash
+## git remote
